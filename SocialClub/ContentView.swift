31ba9859480b08728663545a0157bff730423eaf
Reshid-505 <hreshid2016@gmail.com>
2026-04-07 06:17:18 +0400

    Initial Commit

---
+++ SocialClub/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  SocialClub
+//
+//  Created by Reshid Huseynov on 06.04.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Social Club")
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SocialClub/SocialClubApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialClubApp.swift
+//  SocialClub
+//
+//  Created by Reshid Huseynov on 06.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct SocialClubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
